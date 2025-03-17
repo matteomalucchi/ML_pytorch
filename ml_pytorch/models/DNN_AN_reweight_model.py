Fixed implementation of multiclassifier with softmax (Currently breaks the binary classifier)

ml_pytorch/models/DNN_AN_reweight_model.py
@@ -27,7 +27,7 @@ class DNN(nn.Module):
 
     def forward(self, x):
         logits = self.linear_relu_stack(x)
-        logits = self.softmax(logits)
+        #logits = self.softmax(logits)
         return logits
 
 

ml_pytorch/utils/tools.py
@@ -46,7 +46,8 @@ def loop_one_batch(
 
     # compute the outputs of the model
     outputs = model(inputs)
-    
+    probs = torch.softmax(outputs, dim=1)
+
     if outputs.shape[1] == 1:
         outputs=outputs.flatten()
         y_pred = torch.round(outputs)
@@ -123,7 +124,7 @@ def loop_one_batch(
     if eval_model:
         # Create array of scores and labels
         if i == 0:
-            all_scores = outputs
+            all_scores = torch.softmax(outputs, dim=1)
             all_labels = labels
             all_weights = event_weights
         else:
@@ -163,7 +164,10 @@ def train_val_one_epoch(
     best_model_name=None,
 ):
     logger.info("Training" if train else "Validation")
-    model.train(train)
+    if train: 
+        model.train(train)
+    else:
+        model.eval()
 
     running_loss = 0.0
     tot_loss = 0.0
@@ -339,6 +343,18 @@ def eval_model(model, loader, loss_fn, type, device, best_epoch):
 
 
 def export_onnx(model, model_name, batch_size, input_size, device):
+
+    class ONNXWrappedModel(torch.nn.Module):
+        def __init__(self, original_model):
+            super().__init__()
+            self.model = original_model  # Use the trained model
+
+        def forward(self, x):
+            logits = self.model(x)  # Get raw logits
+            return torch.nn.functional.softmax(logits, dim=1)  # Apply softmax inside ONNX model
+    
+    model = ONNXWrappedModel(model)
+
     # Export the model to ONNX format
     dummy_input = torch.zeros(batch_size, input_size, device=device)
     torch.onnx.export(