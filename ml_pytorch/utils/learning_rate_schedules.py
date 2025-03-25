84299d2d517287cf2eabe3c72c5bab623eec7431
matteomalucchi <matteomalucchi13@gmail.com>
2025-03-26 00:26:12 +0100

    minor change

--- ml_pytorch/utils/learning_rate_schedules.py
+++ ml_pytorch/utils/learning_rate_schedules.py
@@ -12,24 +12,12 @@ def get_linear_schedule_with_warmup(optimizer: Optimizer, total_iters: int):
         optimizer, start_factor=1.0, end_factor=0.0, total_iters=total_iters
     )
 
-def get_delayed_drop_schedule(optimizer: Optimizer):
-    lambda1 = lambda epoch: (0.75**(epoch-20) if epoch>=20 else 1)
+def get_delayed_drop_schedule(optimizer: Optimizer, delay_epochs: int, drop_factor: float):
+    decay_func = lambda epoch: (drop_factor**(epoch-delay_epochs) if epoch>=delay_epochs else 1)
     return LambdaLR(
-        optimizer, lr_lambda=lambda1
+        optimizer, lr_lambda=decay_func
     )
-
-def get_e20_drop95_schedule(optimizer: Optimizer):
-    lambda1 = lambda epoch: (0.95**(epoch-20) if epoch>=20 else 1)
-    return LambdaLR(
-        optimizer, lr_lambda=lambda1
-    )
-
-def get_e30_drop95_schedule(optimizer: Optimizer):
-    lambda1 = lambda epoch: (0.95**(epoch-30) if epoch>=30 else 1)
-    return LambdaLR(
-        optimizer, lr_lambda=lambda1
-    )
-
+    
 def get_lr_scheduler(lr_schedule, optimizer, n_epochs):
     if lr_schedule == "constant":
         scheduler = get_constant_schedule(optimizer)
@@ -37,14 +25,14 @@ def get_lr_scheduler(lr_schedule, optimizer, n_epochs):
         scheduler = get_linear_schedule_with_warmup(
             optimizer, total_iters=n_epochs
         )
-    elif lr_schedule == "delayed_drop":
-        scheduler = get_delayed_drop_schedule(optimizer)
+    elif lr_schedule == "e20_drop75":
+        scheduler = get_delayed_drop_schedule(optimizer, 20, 0.75)
+    elif lr_schedule == "e5_drop75":
+        scheduler = get_delayed_drop_schedule(optimizer, 5, 0.75)
     elif lr_schedule == "e20_drop95":
-        scheduler = get_e20_drop95_schedule(optimizer)
+        scheduler = get_delayed_drop_schedule(optimizer, 20, 0.95)
     elif lr_schedule == "e30_drop95":
-        scheduler = get_e30_drop95_schedule(optimizer)
-
-
+        scheduler = get_delayed_drop_schedule(optimizer, 30, 0.95)
     else:
         raise ValueError("Invalid learning rate schedule")
 
